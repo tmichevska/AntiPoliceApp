69847069b2b63edd9a6516a95036cd88bb79df2f
Tamara Michevska <73593684+tmichevska@users.noreply.github.com>
2022-04-27 11:39:18 +0200

    Create User.java

---
+++ src/main/java/com/example/antipolice/model/User.java
@@ -0,0 +1,64 @@
+package com.example.antipolice.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Data
+@Table(name = "anti_police_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long userId;
+
+    String username;
+
+    String password;
+
+    int age;
+
+
+    @Enumerated(value = EnumType.STRING)
+    private UserType type;
+
+    public User() {
+    }
+
+    public User(String username, String password, int age) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.type = UserType.ROLE_USER;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(type);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
